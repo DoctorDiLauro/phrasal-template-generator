0e0020b173a28ac977cec8b37a3991b6a23ec73d
DoctorDiLauro <chrisdilauro@Chriss-MacBook-Pro-3.local>
2017-05-02 14:02:04 -0400

    changed if statement argument

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verbed = prompt('A past tense verb (ran, burped, etc.)');
 var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\n\nEnter the letter below:');
 
-if (prompt = 'a') {
+if (story = 'a') {
   document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
 }
 
-if (prompt = 'b') {
-  document.write('Amelia' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature.)
+if (story = 'b') {
+  document.write('Amelia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature);
 }
 
-if (prompt = 'c') {
-  document.write('Jackson chose his' + adjective + ', ' + colour + creature + ' card and ' + verbed + ' it to the table knowing he won.')
+if (story = 'c') {
+  document.write('Jackson chose his' + adjective + ', ' + colour + creature + ' card and ' + verbed + ' it to the table knowing he won.');
 }
